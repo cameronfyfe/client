2fcb404a0080b9f3b1065b4024b21e12db23bbb8
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-01-19 21:42:41 -0700

    add nix shell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ { self, ... }:
+    (inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        nodeAndYarn = nodejs: [ nodejs (pkgs.yarn.override { inherit nodejs; }) ];
+
+        nodeAndYarn16 = nodeAndYarn pkgs.nodejs-16_x;
+
+      in
+      rec {
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = nodeAndYarn16;
+          };
+        };
+
+      }));
+}
