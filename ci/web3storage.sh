2fc073d033b9475461a63baca07110af8f382520
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-01-25 12:13:14 -0700

    update ci

--- ci/web3storage.sh
+++ ci/web3storage.sh
@@ -7,7 +7,7 @@ source .env_car
 curl -X POST \
     'https://api.web3.storage/car' \
     -H 'accept: application/json' \
-    -H "Authorization: Bearer $WEB3Token" \
+    -H "Authorization: Bearer $WEB3STORAGE_TOKEN" \
     -H 'Content-Type: application/vnd.ipld.car' \
     -o 'ci/webstorage-response.txt' \
     --data-binary '@ci/build.car' \
